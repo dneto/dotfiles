a67f24a07b9d4b462f768771d8b8d87946997afb
Demétrio Menezes Neto <neto.demetrio@gmail.com>
2024-03-31 17:31:49 -0300

    Configure snippets plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -122,4 +122,34 @@ return {
             end)
         end,
     },
+
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp",
+        dependencies = {
+            {
+                "rafamadriz/friendly-snippets",
+                config = function()
+                    require("luasnip.loaders.from_vscode").lazy_load()
+                end,
+            },
+            {
+                "nvim-cmp",
+                dependencies = {
+                    "saadparwaiz1/cmp_luasnip",
+                },
+                opts = function(_, opts)
+                    opts.snippet = {
+                        expand = function(args)
+                            require("luasnip").lsp_expand(args.body)
+                        end,
+                    }
+                    table.insert(opts.sources, { name = "luasnip" })
+                end,
+            },
+        },
+    },
 }
